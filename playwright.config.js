88529bcd9c690c66c5675dc21bc1e40e121e2281
apreda99-star <a.preda99@gmail.com>
2025-08-12 22:30:04 +0200

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -4,8 +4,13 @@ module.exports = defineConfig({
   testDir: './tests',
   reporter: [['html', { outputFolder: 'playwright-report', open: 'never' }]],
   use: {
-    screenshot: 'only-on-failure',
-    video: 'retain-on-failure'
+    // Screenshot e video:
+    // 'only-on-failure' = solo quando fallisce; 'on' = sempre
+    screenshot: 'on',
+    video: 'on',
+    // Tracce (timeline interattiva) - utile per debug
+    trace: 'on-first-retry'
   },
+  // Tutti gli output (screenshot, video, traces) finiranno qui:
   outputDir: 'test-results'
 });
